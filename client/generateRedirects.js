245847f388cd7279d8715b27d2e5af515aae4c5f
itu-itis22-kuyumcus22 <kuyumcus22@itu.edu.tr>
2025-12-20 22:48:12 +0300

    Github Actions setup for server deployment

---
+++ client/generateRedirects.js
@@ -0,0 +1,11 @@
+import fs from 'node:fs';
+
+const backendApiUrl = process.env.BACKEND_API_URL;
+
+const redirectContent = `/api/*  ${backendApiUrl}/api/:splat  200\n`;
+
+if (!fs.existsSync('./public')) {
+    fs.mkdirSync('./public');
+}
+
+fs.writeFileSync('./public/_redirects', redirectContent);
